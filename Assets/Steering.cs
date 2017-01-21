c7ab1d82ee6a246befecb9a487a11e88c4e38ccc
FomTarro <twfarro@wpi.edu>
2017-01-20 22:40:50 -0500

    Initial Commit

---
+++ Assets/Steering.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering : MonoBehaviour {
+
+    Rigidbody _rigidbody;
+
+    public float _speed = 2;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _rigidbody.AddForce(_speed*_rigidbody.mass *transform.forward* (Input.GetAxis("Vertical")));
+	}
+}
