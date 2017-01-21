3667429e984800c5d9932c71631f3091fc61b4c2
bonbombs <kelly.zhang2306@gmail.com>
2017-01-21 02:09:09 -0500

    projectile stuff

---
+++ Assets/Shooter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public float projectileTime;
+	[SerializeField]
+	public GameObject projectile;
+
+	[SerializeField]
+	SpriteRenderer targetSpriteRender;
+	LineRenderer _lineRender;
+
+	private bool m_isTriggerHeldDown = false;
+	private bool shooting = false;
+	private Vector3 startPoint = new Vector3(0, 1, 2);
+	private Vector3 currentPoint;
+
+	// Use this for initialization
+	void Start () {
+		_lineRender = GetComponent<LineRenderer>();
+		currentPoint = startPoint;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetAxisRaw("RightTrigger") != 0) {
+			if(!m_isTriggerHeldDown) {
+				m_isTriggerHeldDown = true;
+				shooting = true;
+				targetSpriteRender.enabled = true;
+				//currentPoint = startPoint; uncomment if the shoot location needs to be reset after each trigger press
+			}
+			
+		}
+		if (Input.GetAxisRaw("RightTrigger") == 0) {
+			if(m_isTriggerHeldDown)
+				Launch();
+		}
+		if(shooting) {
+			float x = Input.GetAxis("Horizontal");
+			float y = Input.GetAxis("Vertical");
+			currentPoint += new Vector3(x, 0, y);
+			GenerateArc(currentPoint);
+		}
+	}
+
+	void GenerateArc(Vector3 point) {
+		targetSpriteRender.transform.position = currentPoint;
+		// _lineRender.SetPositions(new Vector3[]{
+		// 	new Vector3(),
+		// 	new Vector3()
+		// });
+	}
+
+	void Launch() {
+		float v_x = currentPoint.x / projectileTime;
+		float v_z = currentPoint.z / projectileTime;
+		float v_y = (currentPoint.y + (1/2 * Physics.gravity.y * Mathf.Pow(projectileTime, 2f))) / projectileTime;
+		GameObject newProj = Instantiate(projectile, transform.position + (Vector3.up * 2), Quaternion.identity);
+		Rigidbody rb = newProj.GetComponent<Rigidbody>();
+		Vector3 velocity = new Vector3(v_x, v_y, v_z);
+		rb.velocity = velocity;
+		m_isTriggerHeldDown = false;
+		shooting = false;
+		targetSpriteRender.enabled = false;
+	}
+}
--- Assets/Steering.cs
+++ Assets/Steering.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class Steering : MonoBehaviour {
 
-    Rigidbody _rigidbody;
+	public Transform fixedTransform;
 
-    public float _speed = 2;
+	Rigidbody _rigidbody;
+
+	public float _speed = 2;
+	public float _turnSpeed = 100;
 
 	// Use this for initialization
 	void Start () {
-        _rigidbody = GetComponent<Rigidbody>();	
+		_rigidbody = GetComponent<Rigidbody>();	
 	}
-	
+
+
 	// Update is called once per frame
-	void Update () {
-        _rigidbody.AddForce(_speed*_rigidbody.mass *transform.forward* (Input.GetAxis("Vertical")));
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 forward = vertical * Vector3.forward * _speed * _rigidbody.mass;
+		Vector3 right = horizontal * Vector3.right * _turnSpeed; 
+		// float angle = 0.0f;
+		// if(horizontal != 0.0f || vertical != 0.0f) {
+		// 	angle = Mathf.Atan2(vertical, horizontal) * Mathf.Rad2Deg;
+		// 	Debug.Log(angle);
+		// 	if(angle > transform.eulerAngles.y - 0.5f && angle < transform.eulerAngles.y + 0.5f) {
+		// 		 _rigidbody.AddTorque(0, _turnSpeed, 0);
+		// 	}
+		// }
+		_rigidbody.AddForce(forward + right);
+		_rigidbody.AddForceAtPosition(right, transform.right, ForceMode.Force);
+		
 	}
 }
