1385e6c7dccc56fc8e13dd6660653c0cf5ccac75
FomTarro <twfarro@wpi.edu>
2017-01-21 15:53:49 -0500

    Basic steering

--- Assets/Steering.cs
+++ Assets/Steering.cs
@@ -6,15 +6,36 @@ public class Steering : MonoBehaviour {
 
     Rigidbody _rigidbody;
 
+    public GameObject _compass;
+
     public float _speed = 2;
 
 	// Use this for initialization
 	void Start () {
         _rigidbody = GetComponent<Rigidbody>();	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        _rigidbody.AddForce(_speed*_rigidbody.mass *transform.forward* (Input.GetAxis("Vertical")));
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = -Input.GetAxis("Vertical");
+
+        float angle = 0.0f;
+        if (x != 0.0f || y != 0.0f)
+        {
+            angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg + 180;
+            /*
+            _compass.transform.eulerAngles = new Vector3(0f, angle, 0f);
+            _rigidbody.AddTorque(transform.up * 100 * (_compass.transform.eulerAngles.y - transform.eulerAngles.y));
+            */
+
+            transform.eulerAngles = new Vector3(0f, angle, 0f);
+            Debug.Log(angle + " : " );
+
+        }
+
+        _rigidbody.AddForce(_speed * _rigidbody.mass * transform.forward);
+
+    }
 }
---
+++ Assets/WaterCollisions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCollisions : MonoBehaviour
+{
+
+    int arraySize = 8;
+
+    public float dispersionSpeed = 1;
+    public Vector4 Impact1 = new Vector4(0, 0, 0, 0);
+    Vector4[] collisions;
+
+    Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        collisions = new Vector4[arraySize];
+        rend = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Impact1.w >= 0)
+        {
+            Impact1.w -= Time.deltaTime * dispersionSpeed;
+            rend.material.SetVector("_Impact1", Impact1);
+        }
+
+
+
+
+    }
+}
