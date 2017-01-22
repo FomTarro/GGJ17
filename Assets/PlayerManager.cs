241eebc5dc4de282945eed2c020febd3717987cd
bonbombs <kelly.zhang2306@gmail.com>
2017-01-22 11:17:21 -0500

    UI & experimental files

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	public Transform[] m_Players;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 avg = Vector3.zero;
+
+		foreach(Transform t in m_Players) {
+			Vector3 pos = t.position;
+			avg += pos;
+		}
+
+		avg *= (1f/m_Players.Length);
+		transform.position = avg;
+	}
+}
---
+++ Assets/ScrollingTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingTexture : MonoBehaviour {
+
+	public float scrollSpeed = 0.90f;
+	public float scrollSpeed2 = 0.90f;
+
+	Renderer m_renderer;
+
+	void Start() {
+		m_renderer = GetComponent<Renderer>();
+	}
+ 
+	void FixedUpdate() {
+		var offset = Time.time * scrollSpeed;
+		var offset2 = Time.time * scrollSpeed2;
+		m_renderer.material.mainTextureOffset = new Vector2(offset2,-offset);
+	}
+}
