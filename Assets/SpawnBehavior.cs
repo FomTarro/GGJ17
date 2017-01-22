4d7609d55f03a812fca4927b783420101c68969a
bonbombs <kelly.zhang2306@gmail.com>
2017-01-21 20:43:56 -0500

    Added Generic Spawner

---
+++ Assets/SpawnBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBehavior : MonoBehaviour {
+
+	public Spawner Spawner { get; set; }
+
+	public void Deactivate() {
+		if(Spawner != null) {
+			Spawner.ReturnToPool(gameObject);
+		}
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public int maxAvailable;
+	public float frequency;
+	public float spawnHeight;
+	[SerializeField]
+	GameObject itemReference;
+	[SerializeField]
+	GameObject mapReference;
+	private Stack<GameObject> m_itemPool;
+	private Bounds MapBounds;
+	private Bounds ItemBounds;
+
+	// Use this for initialization
+	void Start () {
+		MapBounds = mapReference.GetComponent<Renderer>().bounds;
+		ItemBounds = itemReference.GetComponentInChildren<Renderer>().bounds;
+		m_itemPool = new Stack<GameObject>();
+		for(int i = 0; i < maxAvailable; i++) {
+			GameObject item = Instantiate(itemReference);
+			SpawnBehavior sb = item.AddComponent<SpawnBehavior>();
+			sb.Spawner = this;
+			ReturnToPool(item);	
+		}
+		InvokeRepeating("Spawn", 0f, frequency);
+	}
+
+	Vector3 GetRandomPointInMap() {
+		float x = Random.Range(MapBounds.min.x, MapBounds.max.x);
+		float z = Random.Range(MapBounds.min.z, MapBounds.max.z);
+		return new Vector3(x, spawnHeight, z);
+	}
+
+	void Spawn() {
+		if(m_itemPool.Count > 0) {
+			Vector3 spawnPos = GetRandomPointInMap();
+			while(Physics.CheckSphere(spawnPos, ItemBounds.size.magnitude)) {
+				spawnPos = GetRandomPointInMap();
+			}
+			GameObject item = m_itemPool.Pop();
+			item.transform.position = spawnPos;
+			item.SetActive(true);
+		}
+	}
+
+	public void ReturnToPool(GameObject item) {
+		item.SetActive(false);
+		m_itemPool.Push(item);
+	}
+}
